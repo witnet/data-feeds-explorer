41f2d602f3e8387ad5aa04d98cbce40a868916a3
María Gabaldón Rico <mariag@stampery.co>
2021-06-02 13:46:01 +0200

    feat: implement fieldset

---
+++ packages/ui/components/Fieldset.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="fieldset">
+    <header class="title">{{ title.toUpperCase() }}</header>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Fieldset',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+.fieldset {
+  display: grid;
+  grid-template-rows: max-content 1fr;
+  .title {
+    color: var(--fieldset-title);
+    font-weight: bold;
+    font-size: 16px;
+    padding: 16px 0 16px 0;
+  }
+  .content {
+    padding: 34px;
+    background: var(--fieldset-background);
+    border-radius: 4px;
+  }
+}
+</style>
