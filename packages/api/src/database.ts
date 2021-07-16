b6e09748bb51824c5f596fb946f83844772ce1f6
tommytrg <tomastrg22@gmail.com>
2021-07-16 14:06:17 +0200

    fix(api): set correct defaultauthdb

--- packages/api/src/database.ts
+++ packages/api/src/database.ts
@@ -8,7 +8,7 @@ export class MongoManager {
   async start (uri?: string): Promise<Db | null> {
     const mongoDbUri =
       uri ||
-      `mongodb://${process.env.MONGO_DATABASE_USERNAME}:${process.env.MONGO_DATABASE_PASSWORD}@${process.env.DB_HOSTNAME}:${process.env.MONGO_PORT}`
+      `mongodb://${process.env.MONGO_DATABASE_USERNAME}:${process.env.MONGO_DATABASE_PASSWORD}@${process.env.DB_HOSTNAME}:${process.env.MONGO_PORT}/database`
     return this.connect(mongoDbUri, process.env.MONGO_INITDB_DATABASE as string)
   }
 
