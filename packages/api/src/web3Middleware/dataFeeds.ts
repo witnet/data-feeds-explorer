fb790f35cd7edbcf88b15963fb50968a4a3fed2f
María Gabaldón Rico <mariag@stampery.co>
2021-07-18 11:03:49 +0200

    feat(ui): update chart view

--- packages/api/src/web3Middleware/dataFeeds.ts
+++ packages/api/src/web3Middleware/dataFeeds.ts
@@ -1,68 +1,68 @@
-import { FeedInfo, Network, AbiItem } from '../types'
-import BtcUsdAbi from './abi/BtcUsd.json'
-import ConfluxBtcUsdAbi from './abi/ConfluxBtcUsd.json'
-import ConfluxWitnetRequestBoardProxy from './abi/ConfluxWitnetRequestBoardProxy.json'
-import WitnetRequestBoardProxy from './abi/WitnetRequestBoardProxy.json'
+// import { FeedInfo, Network, AbiItem } from '../types'
+// import BtcUsdAbi from './abi/BtcUsd.json'
+// import ConfluxBtcUsdAbi from './abi/ConfluxBtcUsd.json'
+// import ConfluxWitnetRequestBoardProxy from './abi/ConfluxWitnetRequestBoardProxy.json'
+// import WitnetRequestBoardProxy from './abi/WitnetRequestBoardProxy.json'
 
-export const dataFeeds: Array<FeedInfo> = [
-  {
-    abi: BtcUsdAbi as Array<AbiItem>,
-    address: '0xa3AFD68122a21c7D21Ddd95E5c077f958dA46662',
-    network: Network.Goerli,
-    name: 'btc/usd',
-    label: '$',
-    pollingPeriod: 10000,
-    witnetRequestBoard: {
-      address: '0x0C4be6AA667df48de54BA174bE7948875fdf152B',
-      abi: WitnetRequestBoardProxy as Array<AbiItem>
-    }
-  },
-  {
-    abi: BtcUsdAbi as Array<AbiItem>,
-    address: '0x56834Ff8D4b27db647Da97CA3bd8540f7fA0e89D',
-    network: Network.Kovan,
-    name: 'btc/usd',
-    label: '$',
-    pollingPeriod: 15000,
-    witnetRequestBoard: {
-      address: '0xD9a6d1Ea0d0f4795985725C7Bd40C31a667c033d',
-      abi: WitnetRequestBoardProxy as Array<AbiItem>
-    }
-  },
-  {
-    abi: BtcUsdAbi as Array<AbiItem>,
-    address: '0x49C0BCce51a8B28f92d008394F06d5B259657F33',
-    witnetRequestBoard: {
-      address: '0x9b42b0D80C428B17A5828dF5C2c96454ca54bD04',
-      abi: WitnetRequestBoardProxy as Array<AbiItem>
-    },
-    network: Network.Rinkeby,
-    name: 'btc/usd',
-    label: '$',
-    pollingPeriod: 10000
-  },
-  {
-    abi: ConfluxBtcUsdAbi as Array<AbiItem>,
-    address: '0x83803e703fb38d8682b53C698B7330dfd262E949',
-    witnetRequestBoard: {
-      address: '0x80786fD4878c98EC22559942e533223CaA8B003F',
-      abi: ConfluxWitnetRequestBoardProxy as Array<AbiItem>
-    },
-    network: Network.Conflux,
-    name: 'btc/usd',
-    label: '$',
-    pollingPeriod: 10000
-  },
-  {
-    abi: ConfluxBtcUsdAbi as Array<AbiItem>,
-    address: '0x8aE319e40dE6Cbb2226d4C449375d429d6782739',
-    witnetRequestBoard: {
-      address: '0x80786fD4878c98EC22559942e533223CaA8B003F',
-      abi: ConfluxWitnetRequestBoardProxy as Array<AbiItem>
-    },
-    network: Network.Conflux,
-    name: 'eth/usd',
-    label: '$',
-    pollingPeriod: 10000
-  }
-]
+// export const dataFeeds: Array<FeedInfo> = [
+//   {
+//     abi: BtcUsdAbi as Array<AbiItem>,
+//     address: '0xa3AFD68122a21c7D21Ddd95E5c077f958dA46662',
+//     network: Network.Goerli,
+//     name: 'btc/usd',
+//     label: '$',
+//     pollingPeriod: 10000,
+//     witnetRequestBoard: {
+//       address: '0x0C4be6AA667df48de54BA174bE7948875fdf152B',
+//       abi: WitnetRequestBoardProxy as Array<AbiItem>
+//     }
+//   },
+//   {
+//     abi: BtcUsdAbi as Array<AbiItem>,
+//     address: '0x56834Ff8D4b27db647Da97CA3bd8540f7fA0e89D',
+//     network: Network.Kovan,
+//     name: 'btc/usd',
+//     label: '$',
+//     pollingPeriod: 15000,
+//     witnetRequestBoard: {
+//       address: '0xD9a6d1Ea0d0f4795985725C7Bd40C31a667c033d',
+//       abi: WitnetRequestBoardProxy as Array<AbiItem>
+//     }
+//   },
+//   {
+//     abi: BtcUsdAbi as Array<AbiItem>,
+//     address: '0x49C0BCce51a8B28f92d008394F06d5B259657F33',
+//     witnetRequestBoard: {
+//       address: '0x9b42b0D80C428B17A5828dF5C2c96454ca54bD04',
+//       abi: WitnetRequestBoardProxy as Array<AbiItem>
+//     },
+//     network: Network.Rinkeby,
+//     name: 'btc/usd',
+//     label: '$',
+//     pollingPeriod: 10000
+//   },
+//   {
+//     abi: ConfluxBtcUsdAbi as Array<AbiItem>,
+//     address: '0x83803e703fb38d8682b53C698B7330dfd262E949',
+//     witnetRequestBoard: {
+//       address: '0x80786fD4878c98EC22559942e533223CaA8B003F',
+//       abi: ConfluxWitnetRequestBoardProxy as Array<AbiItem>
+//     },
+//     network: Network.Conflux,
+//     name: 'btc/usd',
+//     label: '$',
+//     pollingPeriod: 10000
+//   },
+//   {
+//     abi: ConfluxBtcUsdAbi as Array<AbiItem>,
+//     address: '0x8aE319e40dE6Cbb2226d4C449375d429d6782739',
+//     witnetRequestBoard: {
+//       address: '0x80786fD4878c98EC22559942e533223CaA8B003F',
+//       abi: ConfluxWitnetRequestBoardProxy as Array<AbiItem>
+//     },
+//     network: Network.Conflux,
+//     name: 'eth/usd',
+//     label: '$',
+//     pollingPeriod: 10000
+//   }
+// ]
