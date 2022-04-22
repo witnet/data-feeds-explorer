b55cd6b552dbf8f0b10d82157d0a4c82bb9f2003
gabaldon <maria@otherplane.com>
2022-04-22 16:44:59 +0200

    feat(ui): order sidebar options alphabetically

--- packages/ui/utils/generateNavOptions.js
+++ packages/ui/utils/generateNavOptions.js
@@ -1,3 +1,5 @@
 export function generateNavOptions(list) {
-  return list.map((network) => network[0].chain)
+  return list
+    .map((network) => network[0].chain)
+    .sort((a, b) => (b < a ? 1 : -1))
 }
