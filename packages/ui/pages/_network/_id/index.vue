f042591546b16d26445a9c82381c4c486635fe67
gabaldon <maria@otherplane.com>
2022-08-31 13:33:06 +0200

    feat(ui): add dynamic meta tags

--- packages/ui/pages/_network/_id/index.vue
+++ packages/ui/pages/_network/_id/index.vue
@@ -1,3 +1,77 @@
 <template>
-  <DataFeedDetails />
+  <DataFeedDetails
+    @feed-name="setFeedName"
+    @feed-value="setFeedValue"
+    @feed-date="setFeedDate"
+  />
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      currentFeedName: '',
+      lastResultValue: '',
+      lastResultDate: '',
+    }
+  },
+  i18n: {
+    seo: true,
+  },
+  head() {
+    return {
+      title: `${this.currentFeedName} Witnet Data Feed on ${this.selectedDataFeed}`,
+      meta: [
+        {
+          hid: 'title',
+          name: 'title',
+          content: `${this.currentFeedName} Witnet Data Feed on ${this.selectedDataFeed}`,
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: `Last result of ${this.currentFeedName} Witnet Data Feed on ${this.selectedDataFeed} is ${this.lastResultValue} at ${this.lastResultDate}`,
+        },
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: `${this.currentFeedName} Witnet Data Feed on ${this.selectedDataFeed}`,
+        },
+        {
+          hid: 'twitter:description',
+          name: 'twitter:description',
+          content: `Last result of ${this.currentFeedName} Witnet Data Feed on ${this.selectedDataFeed} is ${this.lastResultValue} at ${this.lastResultDate}`,
+        },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: `${this.currentFeedName} Witnet Data Feed on ${this.selectedDataFeed}`,
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: `Last result of ${this.currentFeedName} Witnet Data Feed on ${this.selectedDataFeed} is ${this.lastResultValue} at ${this.lastResultDate}`,
+        },
+      ],
+    }
+  },
+  computed: {
+    selectedDataFeed() {
+      return this.$store.state.selectedNetwork
+        ? this.$store.state.selectedNetwork[0]?.label
+        : null
+    },
+  },
+  methods: {
+    setFeedName(name) {
+      this.currentFeedName = name
+    },
+    setFeedValue(value) {
+      this.lastResultValue = value
+    },
+    setFeedDate(date) {
+      this.lastResultDate = date
+    },
+  },
+}
+</script>
