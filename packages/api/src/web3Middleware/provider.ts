1780d3a533661e869e8c8cfc18b336c070d7b29f
tommy <tomastrg22@gmail.com>
2022-10-31 12:24:20 +0100

    chore(api): remove unnecessary replace on network name

--- packages/api/src/web3Middleware/provider.ts
+++ packages/api/src/web3Middleware/provider.ts
@@ -2,5 +2,5 @@ import { Network } from './../types'
 
 export function getProvider (network: Network) {
   // The provider env variable name should be the network name specified in the config plus '_PROVIDER' ex.MOONBEAM_MOONBASE_PROVIDER
-  return process.env[`${network.toUpperCase().replace('-', '_')}_PROVIDER`]
+  return process.env[`${network.toUpperCase()}_PROVIDER`]
 }
